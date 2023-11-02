b3e431abbcce83c11c2cc5cc410b8a597706acac
firhzal <149083924+firhzal@users.noreply.github.com>
2023-11-02 16:18:24 +0100

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,18 +1,1 @@
 const staticDevCoffee = "dev-coffee-site-v1";
-
-
-self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
-      cache.addAll(assets);
-    })
-  );
-});
-
-self.addEventListener("fetch", fetchEvent => {
-  fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(res => {
-      return res || fetch(fetchEvent.request);
-    })
-  );
-});
